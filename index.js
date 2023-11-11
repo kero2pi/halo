ef538c5e6b433ac9879666c2405fa9e285049b8d
kero2pi <127638505+kero2pi@users.noreply.github.com>
2023-11-11 19:22:08 +0000

    test nodemon

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/test', (req, res) => {
+    res.send('New Route!')
+})
+
+app.get('/lorem/ipsum/akis/mait', (req, res) => {
+    res.send({
+        data: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro ea exercitationem unde maiores! Consequatur nobis asperiores impedit perferendis, veniam facere? Obcaecati inventore ipsum consequatur voluptates."
+    })
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
